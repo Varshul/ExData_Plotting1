0642d39bd75bcbacc58960d2857705cd6f61579d
Varshul <varshul.gupta@gmail.com>
2014-12-03 23:39:05 +0530

    Delete plot1.R

--- plot1.R
+++
@@ -1,8 +0,0 @@
-a <- read.table("household_power_consumption.txt",sep=";",header=TRUE,na.strings="?")
-a$Date <- as.Date(a$Date,"%d/%m/%Y")
-d <- a[which(a$Date =="2007-02-01" | a$Date == "2007-02-02"),]
-d$Time <- strptime(d$Time,"%H:%M:%S")
-
-png(file="plot1.png")
-with(faithful,hist(d$Global_active_power,col = "red",xlab="Global Active Power (kilowatts)",ylab="Frequency",main="Global Active Power"))
-dev.off()
